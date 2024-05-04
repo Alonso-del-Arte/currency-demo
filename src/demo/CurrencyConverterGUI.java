2bcbcba972976b6ce39792608e17d3d5e91c9841
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-03 21:41:12 -0400

    New class, new test class

---
+++ src/demo/CurrencyConverterGUI.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package demo;
+
+import currency.CurrencyConverter;
+import currency.MoneyAmount;
+import draft.ConverterDisplay;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
+import java.util.Locale;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class CurrencyConverterGUI extends JFrame implements ActionListener, 
+        ItemListener {
+    
+    private static final Currency DOLLARS = Currency.getInstance(Locale.US);
+    
+    private static final Currency EUROS = Currency.getInstance("EUR");
+    
+    private static final Currency[] ALL_CURRENCIES 
+            = Currency.getAvailableCurrencies().toArray(Currency[]::new);
+    
+    private final JComboBox<Currency> fromCurrencies 
+            = new JComboBox<>(ALL_CURRENCIES);
+    
+    private final JComboBox<Currency> toCurrencies 
+            = new JComboBox<>(ALL_CURRENCIES);
+    
+    private final JTextField numberField = new JTextField(10);
+    
+    private JLabel fromCurrencyNameReadout 
+            = new JLabel("SORRY, NOT IMPLEMENTED YET");
+    
+    private JLabel toCurrencyNameReadout 
+            = new JLabel("SORRY, NOT IMPLEMENTED YET");
+    
+    private JLabel fromReadout = new JLabel("SORRY, NOT IMPLEMENTED YET");
+    
+    private JLabel toReadout = new JLabel("SORRY, NOT IMPLEMENTED YET");
+    
+    private Currency fromCurrency, toCurrency;
+    
+    private MoneyAmount fromAmount, toAmount;
+    
+    private void updateReadouts() {
+        //
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent ie) {
+        //
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        //
+    }
+
+    public CurrencyConverterGUI(Currency from, Currency to) {
+        this.fromCurrency = from;
+        this.toCurrency = to;
+        this.fromAmount = new MoneyAmount(1, this.fromCurrency);
+        this.updateReadouts();
+        String title = "Currency conversion from " 
+                + this.fromCurrency.getCurrencyCode() + " to " 
+                + this.toCurrency.getCurrencyCode();
+        this.setTitle(title);
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(new JLabel("From: "));
+        inputPanel.add(this.numberField);
+        inputPanel.add(this.fromCurrencies);
+        inputPanel.add(new JLabel("To: "));
+        inputPanel.add(this.toCurrencies);
+        this.add(inputPanel, BorderLayout.PAGE_START);
+        JPanel namesPanel = new JPanel(new BorderLayout(20, 20));
+//        this.fromCurrencyNameReadout.setText(from.getDisplayName());
+        namesPanel.add(this.fromCurrencyNameReadout, BorderLayout.PAGE_START);
+        namesPanel.add(this.toCurrencyNameReadout, BorderLayout.PAGE_END);
+        this.add(namesPanel, BorderLayout.CENTER);
+        JPanel readoutsPanel = new JPanel();
+        readoutsPanel.add(this.fromReadout);
+        readoutsPanel.add(new JLabel(" exchanges to "));
+        readoutsPanel.add(this.toReadout);
+        this.add(readoutsPanel, BorderLayout.PAGE_END);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
+    
+    public static void main(String[] args) {
+        CurrencyConverterGUI display = new CurrencyConverterGUI(DOLLARS, EUROS);
+        display.setVisible(true);
+    }
+    
+}
---
+++ test/demo/CurrencyConverterGUINGTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package demo;
+
+import currency.CurrencyConverter;
+import currency.MoneyAmount;
+import draft.ConverterDisplay;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Currency;
+import java.util.Locale;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ * Tests of the CurrencyConverterGUI class.
+ * @author al
+ */
+public class CurrencyConverterGUINGTest {
+    
+    public CurrencyConverterGUINGTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
