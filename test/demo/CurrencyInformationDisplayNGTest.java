c51e424a8c9a8d2db572e1c973028901e663363a
Alonso del Arte <alonso.delarte@gmail.com>
2024-07-05 20:45:48 -0400

    Failing symbol label enabled when symbol different from letter code test

--- test/demo/CurrencyInformationDisplayNGTest.java
+++ test/demo/CurrencyInformationDisplayNGTest.java
@@ -358,6 +358,20 @@ public class CurrencyInformationDisplayNGTest implements ItemListener {
     }
     
     @Test
+    public void testSymbolLabelEnabledWhenSymbolDiffersFromLetterCode() {
+        Currency currency = CurrencyChooser.chooseCurrency(
+                (curr) -> !curr.getSymbol().equals(curr.getCurrencyCode())
+        );
+        CurrencyInformationDisplay instance 
+                = new CurrencyInformationDisplay(currency);
+        String msg = "Given that " + currency.getDisplayName() + " (" 
+                + currency.getCurrencyCode() + ") has symbol " 
+                + currency.getSymbol() 
+                + " in this locale, symbol label should be enabled";
+        assert instance.symbolLabel.isEnabled() : msg;
+    }
+    
+    @Test
     public void testFractionDigitsPerCurrencySpecifiedInConstructor() {
         Currency currency = CurrencyChooser.chooseCurrency();
         CurrencyInformationDisplay instance 
