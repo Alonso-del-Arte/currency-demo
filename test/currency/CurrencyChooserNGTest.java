acc0af6644c6f43e618b12a2103b5bd0a7062f1b
Alonso del Arte <alonso.delarte@gmail.com>
2024-07-24 16:07:02 -0400

    Added Spanish peseta (ESP) to list of euro-replaced currencies; failing three tests

--- test/currency/CurrencyChooserNGTest.java
+++ test/currency/CurrencyChooserNGTest.java
@@ -56,7 +56,7 @@ public class CurrencyChooserNGTest {
             * TOTAL_NUMBER_OF_CURRENCIES;
     
     private static final String[] EURO_REPLACED_EXCLUSION_CODES = {"ADP", "CYP", 
-        "EEK", "FIM", "GRD", "ITL", "LUF", "MTL", "NLG", "PTE", "SIT"};
+        "EEK", "ESP", "FIM", "GRD", "ITL", "LUF", "MTL", "NLG", "PTE", "SIT"};
     
     private static final String[] OTHER_EXCLUSION_CODES = {"BGL", "BOV", "CHE", 
         "CHW", "COU", "MXV", "USN", "USS", "UYI"};
