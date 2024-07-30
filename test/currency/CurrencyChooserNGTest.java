80c734629378d304ba0bb3cf8897cb06c5aaf48e
Alonso del Arte <alonso.delarte@gmail.com>
2024-07-30 16:47:40 -0400

    Added Deutsche Mark (DEM) to list of euro-replaced currencies; failing four tests

--- test/currency/CurrencyChooserNGTest.java
+++ test/currency/CurrencyChooserNGTest.java
@@ -56,8 +56,8 @@ public class CurrencyChooserNGTest {
             * TOTAL_NUMBER_OF_CURRENCIES;
     
     private static final String[] EURO_REPLACED_EXCLUSION_CODES = {"ADP", "BEF", 
-        "CYP", "EEK", "ESP", "FIM", "FRF", "GRD", "IEP", "ITL", "LUF", "MTL", 
-        "NLG", "PTE", "SIT"};
+        "CYP", "DEM", "EEK", "ESP", "FIM", "FRF", "GRD", "IEP", "ITL", "LUF", 
+        "MTL", "NLG", "PTE", "SIT"};
     
     private static final String[] OTHER_EXCLUSION_CODES = {"BGL", "BOV", "CHE", 
         "CHW", "COU", "GWP", "MXV", "USN", "USS", "UYI"};
