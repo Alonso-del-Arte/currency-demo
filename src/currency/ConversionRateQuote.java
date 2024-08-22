0252699f1a0fa8dca3302873c5577522c33cf6f3
Alonso del Arte <alonso.delarte@gmail.com>
2024-08-22 12:47:54 -0400

    Passing constructor rejects null date test

--- src/currency/ConversionRateQuote.java
+++ src/currency/ConversionRateQuote.java
@@ -27,8 +27,8 @@ public class ConversionRateQuote {
     
     public ConversionRateQuote(CurrencyPair currencies, double rate, 
             LocalDateTime date) {
-        if (currencies == null) {
-            String excMsg = "Currency pair should not be null";
+        if (currencies == null || date == null) {
+            String excMsg = "Currency pair, date, should not be null";
             throw new NullPointerException(excMsg);
         }
     }
