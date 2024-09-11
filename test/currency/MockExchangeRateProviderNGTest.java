9e103f9b737d311a4da5ad3d257eca335fd2253a
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-10 23:55:45 -0400

    Failing getRate( ) test

--- test/currency/MockExchangeRateProviderNGTest.java
+++ test/currency/MockExchangeRateProviderNGTest.java
@@ -18,6 +18,7 @@ package currency;
 
 import static currency.CurrencyChooser.RANDOM;
 
+import java.time.LocalDateTime;
 import java.util.Currency;
 import java.util.Locale;
 
@@ -31,5 +32,47 @@ import org.testng.annotations.Test;
  * @author Alonso del Arte
  */
 public class MockExchangeRateProviderNGTest {
+    
+    private static final Currency UNITED_STATES_DOLLARS 
+            = Currency.getInstance(Locale.US);
+    
+    private static ConversionRateQuote[] inventQuotes() {
+        int len = RANDOM.nextInt(16) + 4;
+        ConversionRateQuote[] array = new ConversionRateQuote[len];
+        LocalDateTime date = LocalDateTime.now();
+        for (int i = 0; i < len; i++) {
+            Currency to = CurrencyChooser
+                    .chooseCurrencyOtherThan(UNITED_STATES_DOLLARS);
+            double rate = 1.0 + RANDOM.nextDouble();
+            CurrencyPair currencies = new CurrencyPair(UNITED_STATES_DOLLARS, 
+                    to);
+            date = date.minusDays(i);
+            ConversionRateQuote quote = new ConversionRateQuote(currencies, 
+                    rate, date);
+            array[i] = quote;
+        }
+        return array;
+    }
+    
+    @Test
+    public void testGetRate() {
+        System.out.println("getRate");
+        ConversionRateQuote[] rateQuotes = inventQuotes();
+        double delta = 0.00001;
+        ExchangeRateProvider instance 
+                = new MockExchangeRateProvider(rateQuotes);
+        for (ConversionRateQuote quote : rateQuotes) {
+            CurrencyPair pair = quote.getCurrencies();
+            Currency source = pair.getFromCurrency();
+            Currency target = pair.getToCurrency();
+            double expected = quote.getRate();
+            double actual = instance.getRate(source, target);
+            String message = "Retrieving mock rate of conversion from " 
+                    + source.getDisplayName() + " (" + source.getCurrencyCode() 
+                    + ") to " + target.getDisplayName() + " (" 
+                    + target.getCurrencyCode() + ")";
+            assertEquals(actual, expected, delta, message);
+        }
+    }
     
 }
