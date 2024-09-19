047dad6c31af0470754a67c4cbabebe1c0c5c339
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-19 19:26:13 -0400

    New class

---
+++ test/currency/RateQuoteCacheNGTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2024 al
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package currency;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ * Tests of the RateQuoteCache class.
+ * @author Alonso del Arte
+ */
+public class RateQuoteCacheNGTest {
+    
+    /**
+     * Test of create method, of class RateQuoteCache.
+     */
+    @Test
+    public void testCreate() {
+        System.out.println("create");
+        CurrencyPair currencies = null;
+        RateQuoteCache instance = null;
+        ConversionRateQuote expResult = null;
+        ConversionRateQuote result = instance.create(currencies);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of has method, of class RateQuoteCache.
+     */
+    @Test
+    public void testHas() {
+        System.out.println("has");
+        CurrencyPair currencies = null;
+        RateQuoteCache instance = null;
+        boolean expResult = false;
+        boolean result = instance.has(currencies);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of retrieve method, of class RateQuoteCache.
+     */
+    @Test
+    public void testRetrieve() {
+        System.out.println("retrieve");
+        CurrencyPair currencies = null;
+        RateQuoteCache instance = null;
+        ConversionRateQuote expResult = null;
+        ConversionRateQuote result = instance.retrieve(currencies);
+        assertEquals(result, expResult);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    private static class RateQuoteCacheImpl extends RateQuoteCache {
+
+        @Override
+        public ConversionRateQuote create(CurrencyPair currencies) {
+            return null;
+        }
+        
+        public RateQuoteCacheImpl() {
+            super(10);
+        }
+
+    }
+    
+}
