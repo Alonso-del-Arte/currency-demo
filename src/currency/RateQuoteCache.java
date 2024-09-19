d1be9ade7b58b63ff807d1308d895d274ffb5b2e
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-19 19:44:23 -0400

    Added needsRefresh( ) Boolean function

--- src/currency/RateQuoteCache.java
+++ src/currency/RateQuoteCache.java
@@ -24,12 +24,35 @@ abstract class RateQuoteCache {
     
     private ConversionRateQuote[] quotes = new ConversionRateQuote[10];
     
+    /**
+     * Adds a conversion rate quote to the cache that was not already there. It 
+     * can certainly be the case that a quote for a particular currency pair was 
+     * previously in the cache but was removed for reasons of capacity or 
+     * freshness.
+     * @param currencies The currency pair. For example, United States dollars 
+     * (USD) and euros (EUR).
+     * @return The new conversion rate quote. For example, $1 to &euro;0.89585 
+     * as of September 19, 2024. 
+     */
     abstract ConversionRateQuote create(CurrencyPair currencies);
     
     // TODO: Write tests for this
     boolean has(CurrencyPair currencies) {
         return false;
     }
+    
+    /**
+     * Determines if the conversion rate quote needs to be refreshed. For some 
+     * applications, it might be necessary to refresh quotes from a few minutes 
+     * ago, in others quotes from a month ago might be acceptable. It's even 
+     * possible to distinguish between frequently traded currencies like dollars 
+     * and euros and less commonly traded currencies.
+     * @param currencies The currency pair. For example, United States dollars 
+     * (USD) and euros (EUR).
+     * @return True if the conversion rate quote needs to be refreshed, false if 
+     * not.
+     */
+    abstract boolean needsRefresh(CurrencyPair currencies);
     
     // TODO: Write tests for this
     ConversionRateQuote retrieve(CurrencyPair currencies) {
--- test/currency/RateQuoteCacheNGTest.java
+++ test/currency/RateQuoteCacheNGTest.java
@@ -1,20 +1,22 @@
 /*
- * Copyright (C) 2024 al
+ * Copyright (C) 2024 Alonso del Arte
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package currency;
+
+import java.time.LocalDateTime;
 
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
@@ -75,6 +77,11 @@ public class RateQuoteCacheNGTest {
         @Override
         public ConversionRateQuote create(CurrencyPair currencies) {
             return null;
+        }
+        
+        @Override
+        boolean needsRefresh(CurrencyPair currencies) {
+            return true;
         }
         
         public RateQuoteCacheImpl() {
