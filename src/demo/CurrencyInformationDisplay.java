92aca7054a35e97bc19f46cd7e74a9f779db43ef
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-29 14:29:51 -0400

    Preparing to fail the next test

--- src/demo/CurrencyInformationDisplay.java
+++ src/demo/CurrencyInformationDisplay.java
@@ -128,6 +128,7 @@ public class CurrencyInformationDisplay extends JFrame implements ItemListener {
         panel.add(new JLabel("Choose "));
         this.currenciesDropdown
                 .setSelectedItem(new CurrencyWrapper(this.selectedCurrency));
+        this.currenciesDropdown.setEditable(true);
         panel.add(this.currenciesDropdown);
         panel.add(new JLabel("Currency: "));
         this.displayNameField = new JTextField(this.selectedCurrency
