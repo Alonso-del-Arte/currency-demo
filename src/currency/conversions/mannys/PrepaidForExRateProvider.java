0ec44a4c7243b0d237439577d20ac577956f11b1
Alonso del Arte <alonso.delarte@gmail.com>
2025-02-28 21:45:24 -0500

    New package

---
+++ src/currency/conversions/mannys/PrepaidForExRateProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package currency.conversions.mannys;
+
+import currency.conversions.ExchangeRateProvider;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Currency;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class PrepaidForExRateProvider /* implements ExchangeRateProvider */ {
+    
+    // PLACEHOLDER
+    
+}
