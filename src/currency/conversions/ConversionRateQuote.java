10e23f4b048b7ab092397fb7febb815ec7619a9b
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-05 15:47:16 -0500

    Rewind getDate( ) to failing

--- src/currency/conversions/ConversionRateQuote.java
+++ src/currency/conversions/ConversionRateQuote.java
@@ -59,7 +59,7 @@ public class ConversionRateQuote {
      * p.m. on August 26, 2024.
      */
     public LocalDateTime getDate() {
-        return this.fetchDate;
+        return this.fetchDate.minusYears(100);
     }
     
     /**
