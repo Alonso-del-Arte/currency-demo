1e7ca8c5dda02c57315a38ccb8957e856dc1eb14
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-10 22:48:36 -0400

    Passing supportedCurrencies( ) test

--- src/currency/conversions/ayrtech/FreeAPIAccess.java
+++ src/currency/conversions/ayrtech/FreeAPIAccess.java
@@ -25,6 +25,7 @@ import java.time.Month;
 import java.util.Currency;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * WORK IN PROGRESS... The free API is free to use but it does require an API 
@@ -34,10 +35,32 @@ import java.util.Set;
 public class FreeAPIAccess implements ExchangeRateProvider, 
         SpecificCurrenciesSupport {
     
+    private static final String[] CURRENCY_CODES = {"AED", "AFN", "ALL", "AMD", 
+        "ANG", "AOA", "ARS", "AUD", "AWG", "AZN", "BAM", "BBD", "BDT", "BGN", 
+        "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTN", "BWP", "BYN", 
+        "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CUP", "CVE", 
+        "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN", "ETB", "EUR", "FJD", 
+        "FKP", "GBP", "GEL", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", 
+        "HNL", "HRK", "HTG", "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", 
+        "JMD", "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KRW", "KWD", "KYD", 
+        "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", 
+        "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", 
+        "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", 
+        "PGK", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", 
+        "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SOS", "SRD", 
+        "SSP", "STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", 
+        "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND", 
+        "VUV", "WST", "XAF", "XCD", "XOF", "XPF", "YER", "ZAR", "ZMW"};
+    
+    private static final Set<Currency> SUPPORTED_CURRENCIES 
+            = Set.of(CURRENCY_CODES).stream().map(
+                    currencyCode -> Currency.getInstance(currencyCode)
+            ).collect(Collectors.toSet());
+
     // TODO: Write tests for this
     @Override
     public Set<Currency> supportedCurrencies() {
-        return new HashSet<>();
+        return SUPPORTED_CURRENCIES;
     }
     
     // TODO: Write tests for this
