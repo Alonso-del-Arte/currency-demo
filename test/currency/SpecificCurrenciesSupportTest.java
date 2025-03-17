91863d83a0af4d6ec409b741d35068bdf1e3835e
Alonso del Arte <alonso.delarte@gmail.com>
2025-03-17 18:11:59 -0400

    New test class

---
+++ test/currency/SpecificCurrenciesSupportTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package currency;
+
+import java.util.Currency;
+import java.util.Set;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ * Tests of the SpecificCurrenciesSupport class.
+ * @author Alonso del Arte
+ */
+public class SpecificCurrenciesSupportTest {
+    
+    private static final Set<Currency> ALL_CURRENCIES 
+            = Currency.getAvailableCurrencies();
+    
+    private static class SpecificCurrenciesSupportImpl 
+            implements SpecificCurrenciesSupport {
+        
+        private final Set<Currency> supportedCurrencies;
+        
+        @Override
+        public Set<Currency> supportedCurrencies() {
+            return this.supportedCurrencies;
+        }
+        
+        private SpecificCurrenciesSupportImpl(Set<Currency> currencies) {
+            this.supportedCurrencies = currencies;
+        }
+        
+    }
+    
+}
