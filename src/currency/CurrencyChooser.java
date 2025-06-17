2518c646afb50fe1e7b7a813eadc19e6390a7a3a
Alonso del Arte <alonso.delarte@gmail.com>
2025-06-16 22:21:34 -0400

    Back to passing chooseCurrency( ) test

--- src/currency/CurrencyChooser.java
+++ src/currency/CurrencyChooser.java
@@ -286,7 +286,7 @@ public class CurrencyChooser {
      */
     public static Currency chooseCurrency() {
         int index = RANDOM.nextInt(CURRENCIES.size());
-        return CURRENCIES.get(0);
+        return CURRENCIES.get(index);
     }
 
     /**
