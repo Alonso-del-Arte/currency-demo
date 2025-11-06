19ba50df32178a25441187f0955ccb4ab7b76444
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-05 21:27:27 -0500

    Failing constructor rejects capacity 0 test

--- test/currency/conversions/InvertibleRateQuoteCacheNGTest.java
+++ test/currency/conversions/InvertibleRateQuoteCacheNGTest.java
@@ -86,6 +86,25 @@ public class InvertibleRateQuoteCacheNGTest {
         System.out.println("\"" + excMsg + "\"");
     }
 
+    @Test
+    public void testConstructorRejectsCapacityZero() {
+        int capacity = 0;
+        String msg = "Capacity " + capacity + " should cause an exception";
+        Throwable t = assertThrows(() -> {
+            RateQuoteCache badCache 
+                    = new InvertibleRateQuoteCacheImpl(capacity);
+            System.out.println(msg + ", not given instance " 
+                    + badCache.toString());
+        }, IllegalArgumentException.class, msg);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        String numStr = Integer.toString(capacity);
+        String capMsg = "Exception message should contain \"" + numStr + "\"";
+        assert excMsg.contains(numStr) : capMsg;
+        System.out.println("\"" + excMsg + "\"");
+    }
+    
     private static class InvertibleRateQuoteCacheImpl 
             extends InvertibleRateQuoteCache {
 
