9d3bb4eddb23cd114705b3dfb4801267a82084ea
Alonso del Arte <alonso.delarte@gmail.com>
2025-11-17 17:13:42 -0500

    New class

---
+++ src/currency/comparators/HistoricalCurrenciesComparator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package currency.comparators;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.Year;
+import java.util.Comparator;
+import java.util.Currency;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class HistoricalCurrenciesComparator implements Comparator<Currency> {
+    
+    // TODO: Write tests for this
+    @Override
+    public int compare(Currency currencyA, Currency currencyB) {
+        return 0;
+    }
+    
+}
