6c63dc0d2a82af2913cf1dde35cd5483a5568266
Alonso del Arte <alonso.delarte@gmail.com>
2025-12-01 18:23:48 -0500

    Temporary rewind to failing

--- src/currency/conversions/ayrtech/FreeAPIAccess.java
+++ src/currency/conversions/ayrtech/FreeAPIAccess.java
@@ -264,38 +264,38 @@ public class FreeAPIAccess implements ExchangeRateProvider,
     // TODO: Write tests for this
     @Override
     public double getRate(Currency source, Currency target) {
-        if (source.getCurrencyCode().equals("USD")) {
-            if (target.getCurrencyCode().equals("XCD")) {
-                return 2.7;
-            }
-        }
-        if (source.getCurrencyCode().equals("XCD") 
-                && target.getCurrencyCode().equals("USD")) {
-            return 0.37037037037037035;
-        }
-        return 1.0;
+//        if (source.getCurrencyCode().equals("USD")) {
+//            if (target.getCurrencyCode().equals("XCD")) {
+//                return 2.7;
+//            }
+//        }
+//        if (source.getCurrencyCode().equals("XCD") 
+//                && target.getCurrencyCode().equals("USD")) {
+//            return 0.37037037037037035;
+//        }
+        return -1.0;
     }
     
     // TODO: Write tests for this
     @Override
     public double getRate(CurrencyPair currencies) {
-        if (currencies.getFromCurrency().getCurrencyCode()
-                .equals("USD")) {
-            if (currencies.getToCurrency().getCurrencyCode().equals("XCD")) {
-                return 2.7;
-            } else {
-                if (DOLLAR_CONVERSIONS_MAP.containsKey(currencies)) {
-                    ConversionRateQuote quote 
-                            = DOLLAR_CONVERSIONS_MAP.get(currencies);
-                    return quote.getRate();
-                }
-            }
-        }
-        if (currencies.getFromCurrency().getCurrencyCode().equals("XCD") 
-                && currencies.getToCurrency().getCurrencyCode().equals("USD")) {
-            return 0.37037037037037035;
-        }
-        return 1.0;
+//        if (currencies.getFromCurrency().getCurrencyCode()
+//                .equals("USD")) {
+//            if (currencies.getToCurrency().getCurrencyCode().equals("XCD")) {
+//                return 2.7;
+//            } else {
+//                if (DOLLAR_CONVERSIONS_MAP.containsKey(currencies)) {
+//                    ConversionRateQuote quote 
+//                            = DOLLAR_CONVERSIONS_MAP.get(currencies);
+//                    return quote.getRate();
+//                }
+//            }
+//        }
+//        if (currencies.getFromCurrency().getCurrencyCode().equals("XCD") 
+//                && currencies.getToCurrency().getCurrencyCode().equals("USD")) {
+//            return 0.37037037037037035;
+//        }
+        return -1.0;
     }
     
     /**
