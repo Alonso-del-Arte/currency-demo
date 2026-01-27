b361fb44379bafe474290f67ba0c39be63b24c94
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-26 23:55:27 -0500

    Failing year span test for French franc

--- test/currency/CurrencyYearSpanDeterminerNGTest.java
+++ test/currency/CurrencyYearSpanDeterminerNGTest.java
@@ -195,4 +195,16 @@ public class CurrencyYearSpanDeterminerNGTest {
         assertEquals(actual, expected, message);
     }
     
+    @Test
+    public void testDetermineYearSpanFrenchFranc() {
+        Currency currency = Currency.getInstance("FRF");
+        Year begin = Year.of(1960);
+        YearSpan expected = new YearSpan(begin, EURO_YEAR_ZERO);
+        YearSpan actual 
+                = CurrencyYearSpanDeterminer.determineYearSpan(currency);
+        String message = "Currency " + currency.getDisplayName() + " (" 
+                + currency.getCurrencyCode() + ") is a euro-replaced currency";
+        assertEquals(actual, expected, message);
+    }
+    
 }
