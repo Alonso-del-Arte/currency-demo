75a4563c6126e7b0a0022e2e0b6d5b992c5c5f8c
Alonso del Arte <alonso.delarte@gmail.com>
2026-01-27 00:37:49 -0500

    Failing year span test for Irish pound

--- test/currency/CurrencyYearSpanDeterminerNGTest.java
+++ test/currency/CurrencyYearSpanDeterminerNGTest.java
@@ -219,4 +219,16 @@ public class CurrencyYearSpanDeterminerNGTest {
         assertEquals(actual, expected, message);
     }
     
+    @Test
+    public void testDetermineYearSpanIrishPound() {
+        Currency currency = Currency.getInstance("IEP");
+        Year begin = Year.of(1928);
+        YearSpan expected = new YearSpan(begin, EURO_YEAR_ZERO);
+        YearSpan actual 
+                = CurrencyYearSpanDeterminer.determineYearSpan(currency);
+        String message = "Currency " + currency.getDisplayName() + " (" 
+                + currency.getCurrencyCode() + ") is a euro-replaced currency";
+        assertEquals(actual, expected, message);
+    }
+    
 }
