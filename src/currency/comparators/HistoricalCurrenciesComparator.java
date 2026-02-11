82a57c67eb0c42a833a81fc2819a8d166dab75c3
Alonso del Arte <alonso.delarte@gmail.com>
2026-02-10 23:33:12 -0500

    Javadoc

--- src/currency/comparators/HistoricalCurrenciesComparator.java
+++ src/currency/comparators/HistoricalCurrenciesComparator.java
@@ -24,11 +24,41 @@ import java.util.Currency;
 import timeops.YearSpan;
 
 /**
- *
+ * Compares currencies according to their year spans of validity, prioritizing 
+ * currencies that are no longer active. Some historical currencies, such as the 
+ * old Sudanese dinar (SDD), have their years of validity (for example, 1992 
+ * &mdash; 2007, in the case of the old Sudanese dinar) listed in Java's 
+ * currency information file. This is not the case with euro-replaced 
+ * currencies.
  * @author Alonso del Arte
  */
 public class HistoricalCurrenciesComparator implements Comparator<Currency> {
     
+    /**
+     * Compares two currencies according to their years of validity. The years 
+     * of validity are gleaned from the Java currency information file when 
+     * possible. For euro-replaced currencies, the years of validity have been 
+     * "hard-coded" in this program. Currently active currencies like the United 
+     * States dollar (USD) are considered to have a range of validity starting 
+     * with the present year. This is so that currently active currencies will 
+     * be sorted after historical currencies.
+     * @param currencyA The first currency to compare. For example, the present 
+     * day Venezuelan bol&iacute;var (VES).
+     * @param currencyB The second currency to compare. For example, the 
+     * Venezuelan bol&iacute;var from 1871 to 2008 (VEB).
+     * @return &minus;1 or any other negative integer if {@code currencyA} is 
+     * historical and {@code currencyB} is a currently active currency or if 
+     * both currencies are historical but {@code currencyA} has a span of years 
+     * of validity beginning prior to those of {@code currencyB}; 0 if both 
+     * currencies are currently active currencies, or, less likely, both are 
+     * historical currencies with the exact same years of validity; 1 or any 
+     * other positive integer if {@code currencyA} is a currently active 
+     * currency and {@code currencyB} is historical, or if both are historical 
+     * but {@code currencyA} has a span of years of validity beginning after 
+     * those of {@code currencyB}. In the example with VES and VEB, the result 
+     * could be 1 or 155 or some other positive integer, since VES is currently 
+     * active while VEB was valid from 1871 to 2008.
+     */
     @Override
     public int compare(Currency currencyA, Currency currencyB) {
         YearSpan spanA = determineYearSpan(currencyA);
