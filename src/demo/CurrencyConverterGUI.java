4eae17222e65106ed70122ae2b21053aab10a74a
Alonso del Arte <alonso.delarte@gmail.com>
2026-06-01 22:36:28 -0400

    Passing default close operation test

--- src/demo/CurrencyConverterGUI.java
+++ src/demo/CurrencyConverterGUI.java
@@ -157,7 +157,7 @@ public class CurrencyConverterGUI extends JFrame implements ActionListener,
         readoutsPanel.add(new JLabel(" exchanges to "));
         readoutsPanel.add(this.toReadout);
         this.add(readoutsPanel, BorderLayout.PAGE_END);
-        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.pack();
     }
     
