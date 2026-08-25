c321b64f0dbf65213609e928cd6828237aebdb4f
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-25 16:01:29 -0400

    Failing test that constructor rejects null converter

--- test/demo/CurrencyConverterGUINGTest.java
+++ test/demo/CurrencyConverterGUINGTest.java
@@ -312,4 +312,23 @@ public class CurrencyConverterGUINGTest implements ItemListener {
         System.out.println("\"" + excMsg + "\"");
     }
     
+    @Test
+    public void testConstructorRejectsNullConverter() {
+        Currency from = CurrencyChooser.chooseCurrency(ALLOWED_CURRENCIES);
+        Currency to = CurrencyChooser.chooseCurrencyOtherThan(from, 
+                ALLOWED_CURRENCIES);
+        CurrencyPair currencies = new CurrencyPair(from, to);
+        String msg = "Pair " + currencies.toString() 
+                + " with null converter should cause exception";
+        Throwable t = assertThrows(() -> {
+            JFrame instance = new CurrencyConverterGUI(currencies, null);
+            System.out.println(msg + ", not created instance " 
+                    + instance.toString());
+        }, NullPointerException.class, msg);
+        String excMsg = t.getMessage();
+        assert excMsg != null : "Exception message should not be null";
+        assert !excMsg.isBlank() : "Exception message should not be blank";
+        System.out.println("\"" + excMsg + "\"");
+    }
+    
 }
