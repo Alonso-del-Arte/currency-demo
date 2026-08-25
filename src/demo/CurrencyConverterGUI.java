22b7088f1000c8777ca09a0bc3a2ed4ca5e79be4
Alonso del Arte <alonso.delarte@gmail.com>
2026-08-25 15:40:25 -0400

    Refactored explicit null check of currency pair out

--- src/demo/CurrencyConverterGUI.java
+++ src/demo/CurrencyConverterGUI.java
@@ -157,10 +157,6 @@ public class CurrencyConverterGUI extends JFrame implements ActionListener,
 
     public CurrencyConverterGUI(CurrencyPair currencies, 
             CurrencyConverter converter) {
-        if (currencies == null) {
-            String excMsg = "Null currency pair is not valid";
-            throw new NullPointerException(excMsg);
-        }
         this.curPair = currencies;
         Currency from = currencies.getFromCurrency();
         Currency to = currencies.getToCurrency();
