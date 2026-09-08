bf88ed98cad89bec80b48bb28306edca7ed5af02
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-08 19:43:51 -0400

    Failing test that To currencies are limited by rate provider

--- test/demo/CurrencyConverterGUINGTest.java
+++ test/demo/CurrencyConverterGUINGTest.java
@@ -220,6 +220,22 @@ public class CurrencyConverterGUINGTest implements ItemListener {
     }
     
     @Test
+    public void testToCurrenciesAreLimitedByRateProvider() {
+        ConversionRateQuote[] rateQuotes 
+                = MockExchangeRateProviderNGTest.inventQuotes();
+        ExchangeRateProvider rateProvider 
+                = new MockExchangeRateProvider(rateQuotes);
+        CurrencyConverter converter = new CurrencyConverter(rateProvider);
+        CurrencyConverterGUI instance = new CurrencyConverterGUI(converter);
+        Set<CurrencyWrapper> expected = rateProvider.supportedCurrencies()
+                .stream().map(currency -> new CurrencyWrapper(currency))
+                .collect(Collectors.toSet());
+        Set<CurrencyWrapper> actual 
+                = new HashSet<>(listItems(instance.toCurrencies));
+        assertContainsSame(expected, actual);
+    }
+    
+    @Test
     public void testInitialFromToCurrenciesAreInitiallySelectedInDropdowns() {
         Currency from = CurrencyChooser.chooseCurrency(ALLOWED_CURRENCIES);
         Currency to = CurrencyChooser.chooseCurrencyOtherThan(from, 
