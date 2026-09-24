d13580eb9d0ef0014bcb7b24c3c99f603c038232
Alonso del Arte <alonso.delarte@gmail.com>
2026-09-23 21:44:32 -0400

    And also failing visible after activation test for 0-param constructor instance

--- test/demo/CurrencyConverterGUINGTest.java
+++ test/demo/CurrencyConverterGUINGTest.java
@@ -342,6 +342,14 @@ public class CurrencyConverterGUINGTest implements ItemListener {
     // TODO: Test not visible before activation
     
     @Test
+    public void testVisibleAfterActivationZeroParamConstructor() {
+        CurrencyConverterGUI instance = new CurrencyConverterGUI();
+        instance.activate();
+        boolean visible = instance.isVisible();
+        assert visible : "Converter should be visible after activation";
+    }
+    
+    @Test
     public void testVisibleAfterActivationAuxConstructor() {
         CurrencyConverterGUI instance 
                 = new CurrencyConverterGUI(MOCK_CONVERTER);
